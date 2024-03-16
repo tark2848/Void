e98b17605b4524a36d7cbaac187fbf413b8687ca
VOIDEX <82993923+VOIDEX-TG@users.noreply.github.com>
2024-03-16 22:32:09 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID"))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001443337704"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001955462999"))
 
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID"))
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC",
+    "https://t.me/VoidexTg",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VIP_CREATORS")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TG_FRIENDSS")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VoidexTg")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/VoidexTgGroup")
 
 # Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
@@ -79,7 +79,7 @@ AUTO_SUGGESTION_TIME = int(
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
 # Cleanmode time after which bot will delete its old messages from chats
 CLEANMODE_DELETE_MINS = int(
-    getenv("CLEANMODE_MINS", "5")
+    getenv("CLEANMODE_MINS", "20")
 )  # Remember to give value in Seconds
 
 # Get your pyrogram v2 session from @VIP_STRING_ROBOT on Telegram
@@ -113,21 +113,21 @@ clean = {}
 autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/4a9a557ca6b48965ac0ca.png"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/4a9a557ca6b48965ac0ca.png"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/4a9a557ca6b48965ac0ca.png"
+STATS_IMG_URL = "https://telegra.ph/file/4a9a557ca6b48965ac0ca.png"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/4a9a557ca6b48965ac0ca.png"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/4a9a557ca6b48965ac0ca.png"
+STREAM_IMG_URL = "https://telegra.ph/file/4a9a557ca6b48965ac0ca.png"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/4a9a557ca6b48965ac0ca.png"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/4a9a557ca6b48965ac0ca.png"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/4a9a557ca6b48965ac0ca.png"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/4a9a557ca6b48965ac0ca.png"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/4a9a557ca6b48965ac0ca.png"
 
 
 def time_to_seconds(time):
